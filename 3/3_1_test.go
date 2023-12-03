f5dff8cce4177cf8128f74033b18796a052c5d8b
Julian Tölle <julian.toelle97@gmail.com>
2023-12-03 19:09:40 +0100

    chore: bootstrap day 3

---
+++ 3/3_1_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/apricote/advent-of-code-2023/util"
+)
+
+func TestSolveCurrentDay(t *testing.T) {
+	type test struct {
+		input string
+		want  int
+	}
+
+	tests := []test{
+		{input: util.GetExampleInput(), want: 0},
+		//{input: util.GetInput(), want: 0},
+	}
+
+	for i, tc := range tests {
+		t.Run(fmt.Sprintf("test %d", i), func(t *testing.T) {
+			got := SolveCurrentDay(tc.input)
+
+			if tc.want != got {
+				t.Errorf("Expected %d but got %d", tc.want, got)
+			}
+		})
+	}
+}
